d0288a375e17792b8396a18af90f8c0b1132459d
snljty <snljty@gmail.com>
2026-01-28 19:38:53 +0800

    add static version

--- get_gro_connectivity.cpp
+++ get_gro_connectivity.cpp
@@ -384,14 +384,18 @@ Eigen::MatrixXf get_dist_matrix_no_box(const Eigen::MatrixXf &coord1, const Eige
 
 std::pair<Eigen::MatrixXf, Eigen::Vector<char, ncoords> > 
     get_dist_matrix_orthorhombic(const Eigen::MatrixXf &coord1, const Eigen::MatrixXf &coord2, const Eigen::Matrix3f box) {
-    Eigen::MatrixXf dist_matrix;
     // column-major
+    static Eigen::MatrixXf dist_matrix;
+    static Eigen::MatrixXf coords1_expanded_row;
+    static Eigen::MatrixXf coords2_expanded_col;
+    static Eigen::MatrixXf diff;
+    static Eigen::MatrixXf shift_real;
     Eigen::Vector3f diag = box.diagonal();
-    Eigen::MatrixXf coords1_expanded_row = coord1.replicate(coord2.cols(), 1);
+    coords1_expanded_row = coord1.replicate(coord2.cols(), 1);
     coords1_expanded_row.resize(ncoords, coord1.cols() * coord2.cols());
-    Eigen::MatrixXf coords2_expanded_col = coord2.replicate(1, coord1.cols());
-    Eigen::MatrixXf diff = coords2_expanded_col - coords1_expanded_row;
-    Eigen::MatrixXf shift_real = - (diff.array().colwise() / diag.array()).round();
+    coords2_expanded_col = coord2.replicate(1, coord1.cols());
+    diff = coords2_expanded_col - coords1_expanded_row;
+    shift_real = - (diff.array().colwise() / diag.array()).round();
     diff.array() += shift_real.array().colwise() * diag.array();
     dist_matrix = diff.colwise().norm(); // Eigen::RowVector, shape (1, coord1.cols() * coord2.cols())
     Eigen::MatrixXf::Index min_dist_pos;
@@ -403,21 +407,25 @@ std::pair<Eigen::MatrixXf, Eigen::Vector<char, ncoords> >
 
 std::pair<Eigen::MatrixXf, Eigen::Vector<char, ncoords> > 
     get_dist_squared_matrix_orthorhombic(const Eigen::MatrixXf &coord1, const Eigen::MatrixXf &coord2, const Eigen::Matrix3f box) {
-    Eigen::MatrixXf dist_squared_matrix;
     // column-major
+    static Eigen::MatrixXf squared_dist_matrix;
+    static Eigen::MatrixXf coords1_expanded_row;
+    static Eigen::MatrixXf coords2_expanded_col;
+    static Eigen::MatrixXf diff;
+    static Eigen::MatrixXf shift_real;
     Eigen::Vector3f diag = box.diagonal();
-    Eigen::MatrixXf coords1_expanded_row = coord1.replicate(coord2.cols(), 1);
+    coords1_expanded_row = coord1.replicate(coord2.cols(), 1);
     coords1_expanded_row.resize(ncoords, coord1.cols() * coord2.cols());
-    Eigen::MatrixXf coords2_expanded_col = coord2.replicate(1, coord1.cols());
-    Eigen::MatrixXf diff = coords2_expanded_col - coords1_expanded_row;
-    Eigen::MatrixXf shift_real = - (diff.array().colwise() / diag.array()).round();
+    coords2_expanded_col = coord2.replicate(1, coord1.cols());
+    diff = coords2_expanded_col - coords1_expanded_row;
+    shift_real = - (diff.array().colwise() / diag.array()).round();
     diff.array() += shift_real.array().colwise() * diag.array();
-    dist_squared_matrix = diff.colwise().squaredNorm(); // Eigen::RowVector, shape (1, coord1.cols() * coord2.cols())
+    squared_dist_matrix = diff.colwise().squaredNorm(); // Eigen::RowVector, shape (1, coord1.cols() * coord2.cols())
     Eigen::MatrixXf::Index min_dist_pos;
-    float min_dist = dist_squared_matrix.row(0).minCoeff(& min_dist_pos);
-    dist_squared_matrix.resize(coord1.cols(), coord2.cols());
+    float min_dist = squared_dist_matrix.row(0).minCoeff(& min_dist_pos);
+    squared_dist_matrix.resize(coord1.cols(), coord2.cols());
     Eigen::Vector<char, ncoords> shift_min = shift_real.col(min_dist_pos).cast<char>();
-    return std::make_pair(dist_squared_matrix, shift_min);
+    return std::make_pair(squared_dist_matrix, shift_min);
 }
 
 void print_box(const Eigen::Matrix3f &box) {
