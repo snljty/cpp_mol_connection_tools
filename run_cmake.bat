779e1847a5ac5854647a7eba54f442343d31d39e
snljty <snljty@gmail.com>
2026-01-30 10:19:34 +0800

    fix bug of CMAKE_INSTALL_PREFIX

--- run_cmake.bat
+++ run_cmake.bat
@@ -1,4 +1,9 @@
 @echo off
 
 set CMAKE_PREFIX_PATH=C:/eigen-3.4.1;C:/fmt-12.1.0
+mkdir build
+cd build
 cmake .. -G "MinGW Makefiles" -D CMAKE_INSTALL_PREFIX=C:/get_gro_connectivity -LH
+rem cmake --build . -j
+rem cmake --install .
+cd ..
